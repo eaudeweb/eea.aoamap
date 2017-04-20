b5cb61efdea3db3758333aab17e6dd9f325fedd6
eduard-fironda <eduard.fironda@eaudeweb.ro>
2017-04-20 14:52:29 +0300

    updated package information
    * Change: updated package information
      [eduard-fironda]

--- setup.py
+++ setup.py
@@ -14,9 +14,25 @@ setup(
     description="",
     long_description=open("README.rst").read() + "\n" +
                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    author='Eau de Web',
-    author_email='office@eaudeweb.ro',
-    url='http://www.eea.europa.eu/data-and-maps',
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    keywords='EEA Add-ons Plone Zope'
+    author='European Environment Agency: IDM2 A-Team',
+    author_email='eea-edw-a-team-alerts@googlegroups.com',
+    url='https://github.com/eea/eea.aoamap',
+    license='GPL',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
